d9b46e8338e8db118bea1727abd3e544fb6c1b41
admin <write2bunny@gmail.com>
2020-05-18 17:30:47 +0100

    first commit

---
+++ src/main/java/Udemypractice/App.java
@@ -0,0 +1,13 @@
+package Udemypractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Udemypractice/AppiumTest.java
@@ -0,0 +1,17 @@
+package Udemypractice;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NativeAndroid()
+	{
+		System.out.println("NativeAndroid");
+	}
+@Test
+public void IosApps()
+{
+	System.out.println("IosApps");
+}
+
+}
